5854bcbb7eb29269d296fc2eede064c7c43dbedc
zt373622384 <38717058+zt373622384@users.noreply.github.com>
2019-05-08 13:44:21 +0800

    Add files via upload

---
+++ Demo8Test.java
@@ -0,0 +1,12 @@
+package ThreadDemo1;
+
+public class Demo8Test {
+	public static void main(String[] args) {
+		Thread t1 = new Thread(new MyRunnable03(),"线程A");
+		Thread t2 = new Thread(new MyRunnable03(),"线程B");
+		t1.start();
+		t2.start();
+		
+	}
+
+}
